aa7ac7db02d425dce95775af1c334bd24dfc6100
Akachan00 <chaves.mayra@gmail.com>
2025-09-08 15:07:04 -0300

    Added:New EnergySource & EndGame

--- Assets/TimeController.cs
+++ Assets/TimeController.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class TimeController : MonoBehaviour
 {
+    [SerializeField] private bool _isDebug=false;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +14,7 @@ public class TimeController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (!_isDebug) return;
         if (Input.GetKeyDown(KeyCode.Alpha1))
         {
             Time.timeScale = 1f;
